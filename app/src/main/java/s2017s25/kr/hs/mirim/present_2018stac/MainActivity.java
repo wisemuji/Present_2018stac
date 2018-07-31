1f2eff5294d040ddf9f65e04f27403ed866bff4c
wisemuji <wisemuji@gmail.com>
2018-07-31 19:10:21 +0900

    project 생성

---
+++ app/src/main/java/s2017s25/kr/hs/mirim/present_2018stac/MainActivity.java
@@ -0,0 +1,13 @@
+package s2017s25.kr.hs.mirim.present_2018stac;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
