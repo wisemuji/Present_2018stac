5e3dc85fabf326aae4653d9a33b70f3928ae501b
wisemuji <wisemuji@gmail.com>
2018-08-07 14:55:40 +0900

    소스파일 옮겨옴

---
+++ app/src/main/java/s2017s25/kr/hs/mirim/present_2018stac/PTListAdapter.java
@@ -0,0 +1,73 @@
+package s2017s25.kr.hs.mirim.stac2018_present;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class PTListAdapter extends BaseAdapter {
+
+    Context context;
+    ArrayList<ptlist_list_item> list_itemArrayList;
+    ViewHolder viewHolder;
+
+    class ViewHolder{
+        TextView title_textView;
+        TextView PTtime_textView;
+        ImageView menu_imageView;
+        ImageView start_icon_imageYiew;
+
+    }
+
+
+    public PTListAdapter(Context context, ArrayList<ptlist_list_item> list_itemArrayList) {
+        this.context = context;
+        this.list_itemArrayList = list_itemArrayList;
+    }
+
+    @Override
+    public int getCount() {
+        return this.list_itemArrayList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return list_itemArrayList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if(convertView == null){
+            convertView = LayoutInflater.from(context).inflate(R.layout.item,null);
+
+            viewHolder = new ViewHolder();
+            viewHolder.title_textView = (TextView)convertView.findViewById(R.id.title_textView);
+            viewHolder.PTtime_textView = (TextView)convertView.findViewById(R.id.PTtime_textView);
+            viewHolder.start_icon_imageYiew = (ImageView)convertView.findViewById(R.id.start_icon_imageYiew);
+            viewHolder.menu_imageView = (ImageView)convertView.findViewById(R.id.menu_imageView);
+            convertView.setTag(viewHolder);
+        }else {
+            viewHolder = (ViewHolder)convertView.getTag();
+        }
+        viewHolder.title_textView.setText(list_itemArrayList.get(position).getTitle());
+        viewHolder.PTtime_textView.setText(list_itemArrayList.get(position).getPTtime().toString());
+        viewHolder.start_icon_imageYiew.setImageResource(list_itemArrayList.get(position).getStart_icon());
+        viewHolder.menu_imageView.setImageResource(list_itemArrayList.get(position).getMenu());
+        return convertView;
+    }
+
+
+
+
+    }
