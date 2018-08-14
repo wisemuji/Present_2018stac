28dc3cb506b80d1557bc61566831676de8ccca0f
Seungyeon Song <wnzk413@gmail.com>
2018-08-14 15:09:42 +0900

    발 표 시 작 화 면 은 되 엇 다

--- app/src/main/java/s2017s25/kr/hs/mirim/present_2018stac/dialogActivity.java
+++ app/src/main/java/s2017s25/kr/hs/mirim/present_2018stac/dialogActivity.java
@@ -11,7 +11,7 @@ public class dialogActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_popup);
+        setContentView(R.layout.activity_dialog);
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         TextView closebtn = findViewById(R.id.closeBtn);
