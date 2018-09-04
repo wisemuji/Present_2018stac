bf64f9099bb7448e942a63a1558b5aa663d9ce9f
Seungyeon Song <wnzk413@gmail.com>
2018-09-04 15:17:36 +0900

    팝 업 햇 다

---
+++ app/src/main/java/s2017s25/kr/hs/mirim/present_2018stac/activity/ScriptContentInput.java
@@ -0,0 +1,13 @@
+package s2017s25.kr.hs.mirim.present_2018stac.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScriptContentInput extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_script_content_input);
+    }
+}
