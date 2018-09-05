a2f85dd50efec97cbc0b27a6204152bb802805b5
Seungyeon Song <wnzk413@gmail.com>
2018-09-05 12:05:38 +0900

    스크립트와 키포인트 리스트에 요소 추가가 되지 않는다 왜?왜?왜?????????????????????????????????왜???????????????????????????????

--- app/src/main/java/s2017s25/kr/hs/mirim/present_2018stac/activity/keypointInputActivity.java
+++ app/src/main/java/s2017s25/kr/hs/mirim/present_2018stac/activity/keypointInputActivity.java
@@ -3,11 +3,11 @@ package s2017s25.kr.hs.mirim.present_2018stac.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class KeyptInput extends AppCompatActivity {
+public class keypointInputActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_keypt_input);
+        setContentView(R.layout.activity_keypoint_input);
     }
 }
