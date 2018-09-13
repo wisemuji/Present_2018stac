6e665da57752d31864d8d3d1e6ca3c9bf747d6bb
unknown <wnzk413@gmail.com>
2018-09-13 19:30:30 +0900

    숫자 폰트 변경 및 splashActivity 추가

---
+++ app/src/main/java/s2017s25/kr/hs/mirim/present_2018stac/activity/SplashActivity.java
@@ -0,0 +1,15 @@
+package s2017s25.kr.hs.mirim.present_2018stac.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import s2017s25.kr.hs.mirim.present_2018stac.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
