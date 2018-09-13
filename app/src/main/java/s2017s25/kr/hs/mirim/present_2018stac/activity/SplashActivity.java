46c08d8ad78cccceafd0a684d22e519afe5899c5
unknown <wnzk413@gmail.com>
2018-09-13 23:42:21 +0900

    스플래시 화면 추가

--- app/src/main/java/s2017s25/kr/hs/mirim/present_2018stac/activity/SplashActivity.java
+++ app/src/main/java/s2017s25/kr/hs/mirim/present_2018stac/activity/SplashActivity.java
@@ -1,9 +1,16 @@
 package s2017s25.kr.hs.mirim.present_2018stac.activity;
 
+
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 
 import s2017s25.kr.hs.mirim.present_2018stac.R;
+
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -11,5 +18,24 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+        ImageView splash = (ImageView) findViewById(R.id.splash_image);
+        GlideDrawableImageViewTarget gifImage = new GlideDrawableImageViewTarget(splash);
+        Glide.with(this).load(R.drawable.splash).into(gifImage);
+
+
+        startLoading();
+
+    }
+
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 4500);
     }
 }
