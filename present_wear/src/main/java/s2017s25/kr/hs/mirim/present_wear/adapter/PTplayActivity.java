45295d935df052a4669469092fe289f82893adda
unknown <wnzk413@gmail.com>
2018-09-23 03:58:46 +0900

    일단 오늘은 여기까지,,,,,,,,,,,,,,,,,,,,, PTplay화면 xml 아주아주아주우ㅏㅈ 대충 만들엇다 ,,, ㅎㅎ 허나 삶의 끝이 여기가 아님에 감사하는 내가 최후의 웃는~~~~~~~~남자~~~~~~~~~~~

---
+++ present_wear/src/main/java/s2017s25/kr/hs/mirim/present_wear/adapter/PTplayActivity.java
@@ -0,0 +1,15 @@
+package s2017s25.kr.hs.mirim.present_wear.adapter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import s2017s25.kr.hs.mirim.present_wear.R;
+
+public class PTplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ptplay);
+    }
+}
