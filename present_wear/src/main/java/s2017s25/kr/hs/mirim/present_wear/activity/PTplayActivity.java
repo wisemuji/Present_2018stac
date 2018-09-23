adda11e739b5728bb7bb6765d8aa074bebaa8806
unknown <wnzk413@gmail.com>
2018-09-23 17:52:10 +0900

    실수 딱콩~

--- present_wear/src/main/java/s2017s25/kr/hs/mirim/present_wear/activity/PTplayActivity.java
+++ present_wear/src/main/java/s2017s25/kr/hs/mirim/present_wear/activity/PTplayActivity.java
@@ -16,33 +16,6 @@ import s2017s25.kr.hs.mirim.present_wear.model.Presentation;
 
 public class PTplayActivity extends AppCompatActivity {
 
-    private TimerTask mTask;
-    private Timer mTimer;
-    TextView myOutput;
-    TextView ptTitle;
-    TextView myRec;
-    TextView myTitle;
-    ImageButton btnLock;
-    TextView myBtnStart;
-    TextView myBtnRefresh;
-    TextView btnFinish;
-    Button myBtnRec;
-    Vibrator vibe;
-    ConstraintLayout layoutPlay;
-
-    final static int Init =0;
-    final static int Run =1;
-    final static int Pause =2;
-
-    int cur_Status = Init; //현재의 상태를 저장할변수를 초기화함.
-    int myCount=1;
-    long myBaseTime;
-    long myPauseTime;
-    int i=1;
-    String str;
-    Presentation pt;
-    int osVersion;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
