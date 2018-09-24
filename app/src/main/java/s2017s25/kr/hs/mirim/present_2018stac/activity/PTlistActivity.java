d3e5ece3762a2faa236e039770e7fed8774f0d9c
wisemuji <wisemuji@gmail.com>
2018-09-25 01:30:03 +0900

    휴대폰에서 스마트워치로 보내기

--- app/src/main/java/s2017s25/kr/hs/mirim/present_2018stac/activity/PTlistActivity.java
+++ app/src/main/java/s2017s25/kr/hs/mirim/present_2018stac/activity/PTlistActivity.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.media.Image;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,6 +19,16 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.wearable.DataClient;
+import com.google.android.gms.wearable.DataItem;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -27,6 +38,7 @@ import java.util.Locale;
 import s2017s25.kr.hs.mirim.present_2018stac.db.DBHelper;
 import s2017s25.kr.hs.mirim.present_2018stac.Adapter.PTListAdapter;
 import s2017s25.kr.hs.mirim.present_2018stac.R;
+import s2017s25.kr.hs.mirim.present_2018stac.model.KeyPoint;
 import s2017s25.kr.hs.mirim.present_2018stac.model.Presentation;
 import s2017s25.kr.hs.mirim.present_2018stac.item.pt_list_item;
 
@@ -37,6 +49,7 @@ public class PTlistActivity extends AppCompatActivity {
     PTListAdapter myListAdapter;
     ArrayList<pt_list_item> list_itemArrayList;
     DBHelper dbHelper;
+    DataClient dataClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +58,7 @@ public class PTlistActivity extends AppCompatActivity {
 //        ImageView plusbtn = (ImageView) findViewById(R.id.plusbtn);
         dbHelper = new DBHelper(getApplicationContext(), "Presentation.db", null, 1);
 
+        dataClient = Wearable.getDataClient(this);
         listView = (ListView)findViewById(R.id.my_listView);
 
         listRefresh();
@@ -87,6 +101,45 @@ public class PTlistActivity extends AppCompatActivity {
                                 Presentation pt = dbHelper.getPresentation(t.getText().toString());
                                 Intent intent = new Intent(PTlistActivity.this, ptPlayActivity.class);
                                 intent.putExtra("presentation", pt);
+
+                                // 워치랑 연결
+
+                                // 보낼 데이터 생성
+                                PutDataMapRequest putDataMapRequest = PutDataMapRequest.create("/present");
+                                DataMap dataMap = putDataMapRequest.getDataMap();
+                                dataMap.putString("name", pt.getName());
+                                dataMap.putLong("time",pt.getPresentTime());
+                                ArrayList<KeyPoint> keyPoints=pt.getKeyPoints();
+
+                                int keyPointsSize = keyPoints.size();
+                                String[] keyPointNames = new String[keyPointsSize];
+                                long[] keyPointTimes = new long[keyPointsSize];
+                                for(int i=0;i<keyPointsSize;i++){
+                                    keyPointNames[i]=keyPoints.get(i).getName();
+                                    keyPointTimes[i]=keyPoints.get(i).getVibTime();
+                                }
+                                dataMap.putStringArray("keypointnames", keyPointNames);
+                                dataMap.putLongArray("keypointtimes", keyPointTimes);
+
+                                // 데이터 전송
+                                PutDataRequest putDataRequest = putDataMapRequest.asPutDataRequest();
+                                Task<DataItem> task = dataClient.putDataItem(putDataRequest);
+
+                                // 리스너 등록(안해도됨)
+                                task.addOnCompleteListener(new OnCompleteListener<DataItem>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<DataItem> task) {
+                                        Toast.makeText(getApplicationContext(), "전송 완료!", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                                task.addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        e.printStackTrace();
+                                        Toast.makeText(getApplicationContext(), "전송 실패!", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+
                                 startActivity(intent);
                             }
                         });
